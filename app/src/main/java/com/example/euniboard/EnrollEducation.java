faeece999da9a84da822b5cc4824b82f78bb62d9
KubangPawis <xanderfernandezperegrin@gmail.com>
2023-12-08 19:00:54 +0800

    refactor(enrollment): remodel more page elements to be enclosed in layouts

--- app/src/main/java/com/example/euniboard/EnrollEducation.java
+++ app/src/main/java/com/example/euniboard/EnrollEducation.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +20,6 @@ public class EnrollEducation extends AppCompatActivity {
         Button btnNext = findViewById(R.id.btnNext);
         btnNext.setOnClickListener(e -> goToCongratulations(e));
     }
-
     public void goToCongratulations(View v) {
         Intent intent = new Intent(this, EnrollCongratulations.class);
         startActivity(intent);
