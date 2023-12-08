eb495e63393eed31b5e8527320a4e9b26b87ae85
KubangPawis <xanderfernandezperegrin@gmail.com>
2023-12-08 21:56:08 +0800

    feat(view faculty): add viewable faculty widgets fragment if user is enrolled to subjects

--- app/src/main/java/com/example/euniboard/SubjectsAvailableFragment.java
+++ app/src/main/java/com/example/euniboard/SubjectsAvailableFragment.java
@@ -1,28 +1,13 @@
 package com.example.euniboard;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
-import android.renderscript.Allocation;
-import android.renderscript.Element;
-import android.renderscript.RenderScript;
-import android.renderscript.ScriptIntrinsicBlur;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 public class SubjectsAvailableFragment extends Fragment {
     public SubjectsAvailableFragment() {
@@ -39,7 +24,7 @@ public class SubjectsAvailableFragment extends Fragment {
 
         //BLUR BG METHOD
         View rootView = inflater.inflate(R.layout.fragment_subjects_available, container, false);
-        LinearLayout linearSubjects = rootView.findViewById(R.id.linearSubjects);
+        LinearLayout linearSubjects = rootView.findViewById(R.id.linearFaculty);
 
         String[] subjectList = {"OOP", "Art Appreciation", "Swimming"};
 
